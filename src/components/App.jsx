b78f7e5553c45c6374fea7f4cb6cf8e3e9d6160e
Ariana Mirus <63701400+ariana-mirus20@users.noreply.github.com>
2020-07-04 14:29:59 +1200

    can now render out inputs in an unitemised list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addReminder } from '../actions'
+
+
 
 class App extends Component {
+constructor(props) {
+  super(props)
+  this.state = {
+    text: ''
+  }
+}
+
+addReminder() {
+  this.props.addReminder(this.state.text)
+}
+
+renderReminders() {
+  const { reminders } = this.props
+  return (
+    <ul className="list-group col-sm-4">
+      {
+        reminders.map(reminder => {
+          return(
+            <li key={reminder.id} className="list-group-item">
+              <div>{reminder.text}</div>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
   render() {
+    console.log('this.props', this.props)
     return (
       <div className="App">
         <div className="title">
@@ -12,17 +45,26 @@ class App extends Component {
             <input
             className="form-control"
             placeholder="I have to..."
+            onChange={event => this.setState({text: event.target.value})}
             />
           </div>
           <button 
            type="button" 
-           className="btn btn-success">
+           className="btn btn-success"
+           onClick={() => this.addReminder()}>
             Add Reminder
           </button>
         </div>
+        { this.renderReminders() }
       </div>
     )
   }
 }
 
-export default App
+function mapStateToProps(state){
+  return {
+    reminders: state
+  }
+}
+
+export default connect(mapStateToProps, {addReminder})(App)
